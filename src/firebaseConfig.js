3d69290820f615feba9aac6fe1315f2720de1f52
vanshika704 <sharmavanshi704@gmail.com>
2024-06-28 17:04:33 +0530

    ..

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import firebase from 'firebase/app';
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyC8K3pW7oeCGUSaTsZu12HGWE9CivNwdl4",
   authDomain: "feastfriends-53724.firebaseapp.com",
