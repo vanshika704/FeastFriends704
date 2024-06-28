548cd9448c65ca17c91763cb3d6a58abb43ef583
vanshika704 <sharmavanshi704@gmail.com>
2024-06-28 18:32:17 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import { auth } from './firebaseConfig';
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+
 import MainPage from './pages/Page2';
 import Menu from './components/Menu';
 import Partners from './components/partners';
@@ -9,7 +8,7 @@ import MainCoursePage from './pages/Maincourse';
 import StartersPage from './pages/starters';
 import DessertsPage from './pages/Desserts';
 import DrinksPage from './pages/Drinks';
-import { useState } from 'react';
+import Home from './login';
 
 function App() {
   return (
@@ -29,56 +28,5 @@ function App() {
   );
 }
 
-function Home() {
-  return (
-    <div className='login'>
-      <Logo />
-      <Credentials />
-    </div>
-  );
-}
-
-function Logo() {
-  return (
-    <div className="logo"></div>
-  );
-}
-
-function Credentials() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const SignIn = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      alert("User signed in with email and password");
-      navigate("/Page2");
-    } catch (error) {
-      alert(error.message);
-    }
-  };
-
-  const SignInGoogle = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      await signInWithPopup(auth, provider);
-      alert("User signed in with Google");
-      navigate("/Page2");
-    } catch (error) {
-      alert(error.message);
-    }
-  };
-
-  return (
-    <div className="credentials">
-      <input type="text" placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
-      
-      <button className="custom-button" onClick={SignIn}>Sign In with Email</button><br/>
-      <button className="custom-button" onClick={SignInGoogle}>Sign In with Google</button>
-    </div>
-  );
-}
 
 export default App;
---
+++ src/login.jsx
@@ -0,0 +1,56 @@
+import { auth } from './firebaseConfig';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+function Home() {
+    return (
+      <div className='login'>
+        <Logo />
+        <Credentials />
+      </div>
+    );
+  }
+  
+  function Logo() {
+    return (
+      <div className="logo"></div>
+    );
+  }
+  
+  function Credentials() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+  
+    const SignIn = async () => {
+      try {
+        await createUserWithEmailAndPassword(auth, email, password);
+        alert("User signed in with email and password");
+        navigate("/Page2");
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+  
+    const SignInGoogle = async () => {
+      const provider = new GoogleAuthProvider();
+      try {
+        await signInWithPopup(auth, provider);
+        alert("User signed in with Google");
+        navigate("/Page2");
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+  
+    return (
+      <div className="credentials">
+        <input type="text" placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
+        
+        <button className="custom-button" onClick={SignIn}>Sign In with Email</button><br/>
+        <button className="custom-button" onClick={SignInGoogle}>Sign In with Google</button>
+      </div>
+    );
+  }
+  export default Home;
