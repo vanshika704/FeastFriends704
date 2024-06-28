8cf09720c0b9997e43ac5cc5269a6f775bf8a8e0
vanshika704 <sharmavanshi704@gmail.com>
2024-06-28 22:50:19 +0530

    .

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -16,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore and Auth
-export const firestore = getFirestore(app);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
