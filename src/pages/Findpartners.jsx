79f93bc2f3c0c27a2305e1a73505164414d7f889
vanshika704 <sharmavanshi704@gmail.com>
2024-07-06 00:05:44 +0530

    .

--- src/pages/Findpartners.jsx
+++ src/pages/Findpartners.jsx
@@ -1,6 +1,6 @@
 import Cards from "../components/alicecarousal";
 
 function Findpartner(){
-    return (<><div className="data"><Cards/></div></>)
+    return (<><div className="data"><div className="find">Find a Companion..</div><Cards/></div></>)
 }
 export default Findpartner;
