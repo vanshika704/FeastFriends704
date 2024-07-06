2845f1dbeec6f6f0d895a0037a2d0c1b383bde1e
vanshika704 <sharmavanshi704@gmail.com>
2024-07-06 12:22:00 +0530

    login

--- src/login.jsx
+++ src/login.jsx
@@ -48,8 +48,8 @@ function Credentials() {
 
   return (
     <div className="credentials">
-      <input type="text" placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
+      <input className='emailandpassword' type="text" placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" className='emailandpassword' placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} />
       
       <button className="custom-button email-button" onClick={SignIn}>Sign In with Email</button>
       <button className="custom-button google-button" onClick={SignInGoogle}>Sign In with Google</button>
