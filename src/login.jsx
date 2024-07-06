6463ea279a96207509e5c3eaddd6fc3c8fab2260
vanshika704 <sharmavanshi704@gmail.com>
2024-07-06 16:54:03 +0530

    .

--- src/login.jsx
+++ src/login.jsx
@@ -1,8 +1,7 @@
 import { auth } from './firebaseConfig';
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-
 
 function Home() {
   return (
@@ -23,10 +22,10 @@ function Credentials() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
- 
 
   const SignIn = async () => {
     try {
+      await setPersistence(auth, browserLocalPersistence);
       await createUserWithEmailAndPassword(auth, email, password);
       alert("User signed in with email and password");
       navigate("/Page2");
@@ -38,6 +37,7 @@ function Credentials() {
   const SignInGoogle = async () => {
     const provider = new GoogleAuthProvider();
     try {
+      await setPersistence(auth, browserLocalPersistence);
       await signInWithPopup(auth, provider);
       alert("User signed in with Google");
       navigate("/Page2");
