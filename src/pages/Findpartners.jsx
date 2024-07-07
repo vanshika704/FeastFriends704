0733953b9aa32e6179aa76469f58d6c786c768f0
vanshika704 <sharmavanshi704@gmail.com>
2024-07-07 21:32:25 +0530

    done

--- src/pages/Findpartners.jsx
+++ src/pages/Findpartners.jsx
@@ -1,7 +1,15 @@
 
 import Cards from "../components/alicecarousal";
 
+
 function Findpartner(){
-    return (<><div className="find" >Find a Companion..</div><div className="data"><img src="src/assets/image4.jpg" className="imagefind"></img><div className="cardscontainer"><Cards/></div></div></>)
+    return (<><div className="find" ><div>Find a Companion..</div><div> 
+        <input
+        type="text"
+        placeholder="Location"
+        className="location"
+        // value={location}
+        readOnly
+      /></div></div><div className="data"><img src="src/assets/image4.jpg" className="imagefind"></img><div className="cardscontainer"><Cards/></div></div></>)
 }
 export default Findpartner;
