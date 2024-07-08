296d2288a64413c98646d0a9e40e6c86242480e6
vanshika704 <sharmavanshi704@gmail.com>
2024-07-08 19:10:36 +0530

    icons

--- src/pages/Findpartners.jsx
+++ src/pages/Findpartners.jsx
@@ -1,15 +1,12 @@
 
+import { GiWallet } from "react-icons/gi";
 import Cards from "../components/alicecarousal";
-
+import { FaSearchLocation } from "react-icons/fa";
+import { Spacer } from "@chakra-ui/react";
 
 function Findpartner() {
-  return (<><div className="find" ><div>Find a Companion..</div><div>
-    <input
-      type="text"
-      placeholder="Location"
-      className="location"
-      // value={location}
-      readOnly
-    /></div></div><div className="data"><img src="src/assets/image4.jpg" className="imagefind"></img><div className="cardscontainer"><Cards /></div></div></>)
+  return (<><div className="find" ><div>Find a Companion..</div><div className="location">
+    <FaSearchLocation size={30}/><Spacer width={30}/> <GiWallet size={30} />
+    </div></div><div className="data"><img src="src/assets/image4.jpg" className="imagefind"></img><div className="cardscontainer"><Cards /></div></div></>)
 }
 export default Findpartner;
