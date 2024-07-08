b9923987cb84a2352ff2562a5a83a3dca0ddeadd
vanshika704 <sharmavanshi704@gmail.com>
2024-07-08 19:11:53 +0530

    spacing

--- src/pages/Findpartners.jsx
+++ src/pages/Findpartners.jsx
@@ -6,7 +6,7 @@ import { Spacer } from "@chakra-ui/react";
 
 function Findpartner() {
   return (<><div className="find" ><div>Find a Companion..</div><div className="location">
-    <FaSearchLocation size={30}/><Spacer width={30}/> <GiWallet size={30} />
+    <FaSearchLocation size={30}/><Spacer width={40}/> <GiWallet size={30} />
     </div></div><div className="data"><img src="src/assets/image4.jpg" className="imagefind"></img><div className="cardscontainer"><Cards /></div></div></>)
 }
 export default Findpartner;
